ea406118556c651f45d1d209e9cd3018eef5122c
Arthur da Paz <ambrgo@gmail.com>
2023-08-28 20:49:31 -0300

    Added tests on GitHub Actions swift.yml

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let dependency = Target.Dependency.targetItem(name: "Voir", condition: .when(platforms: [.iOS]))
+
 let package = Package(
     name: "Voir",
+    platforms: [.iOS(.v14)],
     products: [
         .library(name: "Voir", type: .static, targets: ["Voir"]),
         .executable(name: "TemplateInstaller", targets: ["TemplateInstaller"])
     ],
     targets: [
-        .target(name: "Voir", exclude: ["../../Support"]),
-        .testTarget(name: "VoirTests", dependencies: ["Voir"]),
+        .target(name: "Voir"),
+        .testTarget(name: "VoirTests", dependencies: [dependency]),
         .executableTarget(name: "TemplateInstaller")
     ]
 )
